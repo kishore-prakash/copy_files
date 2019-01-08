b0bc5a7d436cf583d80cb63f1bbe69a43a4a81cf
Kishore <kishore.balasa@gmail.com>
2019-01-08 15:05:00 +0530

    Initial Commit

---
+++ test.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+import json
+
+with open('selected.json') as file:
+        data = json.load(file)
+
+folders = data["folders"]
+# print(folders)
+for folder in folders:
+        src = folder["src"]
+        dest = folder["dest"]
+        if not os.path.exists(dest):
+                os.makedirs(dest)
+        src_files = folder["files"]
+        print(src_files)
+        for file_name in src_files:
+                src_full_path = os.path.join(src, file_name)
+                dest_fill_path = os.path.join(dest, file_name)
+                if (os.path.isfile(src_full_path)):
+                        shutil.copy(src_full_path, dest_fill_path)
